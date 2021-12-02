7144e9e3cc77bb60232c16e30e187ecc79647223
lemon-mint <ice.lemons.mint@gmail.com>
2021-12-02 23:37:19 +0900

    ast node

---
+++ ast/ast.go
@@ -0,0 +1,42 @@
+package ast
+
+type NodeType int
+
+const (
+	NodeType_RESERVED NodeType = iota
+	NodeType_STRUCT
+	NodeType_ENUM
+	NodeType_ALIAS
+)
+
+type Node struct {
+	Type NodeType
+	Name string
+	File string
+	Line int
+	Col  int
+
+	// Struct
+	Struct *Struct
+	// Enum
+	Enum *Enum
+	// Alias
+	Alias *Alias
+}
+
+type Field struct {
+	Name string
+	Type string
+}
+
+type Struct struct {
+	Fields []*Field
+}
+
+type Enum struct {
+	Enums []string
+}
+
+type Alias struct {
+	Type string
+}
