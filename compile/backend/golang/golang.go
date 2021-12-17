3b43125139314eb4a610ac33966e87f841b67093
lemon-mint <ice.lemons.mint@gmail.com>
2021-12-17 20:23:37 +0900

    Generate

---
+++ compile/backend/golang/golang.go
@@ -0,0 +1,36 @@
+package golang
+
+import (
+	"embed"
+	"io"
+	"sync"
+
+	"github.com/lemon-mint/strtpl"
+	"github.com/lemon-mint/vstruct/ir"
+)
+
+//go:embed codes/* codes/**
+var tpls embed.FS
+
+var cache map[string]*strtpl.TPL = make(map[string]*strtpl.TPL)
+var cacheMutex sync.Mutex
+
+func GetTpl(name string) *strtpl.TPL {
+	cacheMutex.Lock()
+	defer cacheMutex.Unlock()
+	if tpl, ok := cache[name]; ok {
+		return tpl
+	}
+	f, err := tpls.ReadFile(name)
+	if err != nil {
+		panic(err)
+	}
+	t := strtpl.NewTPL(string(f))
+	cache[name] = t
+	return t
+}
+
+func Generate(w io.Writer, i *ir.IR) error {
+	panic("not implemented")
+	return nil
+}
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/lemon-mint/vstruct
 
 go 1.17
 
-require github.com/kr/pretty v0.3.0
+require (
+	github.com/kr/pretty v0.3.0
+	github.com/lemon-mint/strtpl v0.0.0-20211213131050-8b0f0ac592a6
+)
 
 require (
 	github.com/kr/text v0.2.0 // indirect
