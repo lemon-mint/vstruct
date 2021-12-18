e8235c9e89d6927262a1acd172af702760b920e9
lemon-mint <ice.lemons.mint@gmail.com>
2021-12-18 19:18:05 +0900

    update _out

--- _out/main.go
+++ _out/main.go
@@ -214,7 +214,7 @@ func (s Item) Name() string {
 
 type Inventory []byte
 
-func (s Inventory) Slot0() Item {
+func (s Inventory) RightHand() Item {
 	_ = s[15]
 	var __off0 uint64 = uint64(s[0]) |
 		uint64(s[1])<<8 |
@@ -235,7 +235,7 @@ func (s Inventory) Slot0() Item {
 	return Item(s[__off0:__off1])
 }
 
-func (s Inventory) Slot1() Item {
+func (s Inventory) LeftHand() Item {
 	_ = s[23]
 	var __off0 uint64 = uint64(s[8]) |
 		uint64(s[9])<<8 |
@@ -253,174 +253,6 @@ func (s Inventory) Slot1() Item {
 		uint64(s[21])<<40 |
 		uint64(s[22])<<48 |
 		uint64(s[23])<<56
-	return Item(s[__off0:__off1])
-}
-
-func (s Inventory) Slot2() Item {
-	_ = s[31]
-	var __off0 uint64 = uint64(s[16]) |
-		uint64(s[17])<<8 |
-		uint64(s[18])<<16 |
-		uint64(s[19])<<24 |
-		uint64(s[20])<<32 |
-		uint64(s[21])<<40 |
-		uint64(s[22])<<48 |
-		uint64(s[23])<<56
-	var __off1 uint64 = uint64(s[24]) |
-		uint64(s[25])<<8 |
-		uint64(s[26])<<16 |
-		uint64(s[27])<<24 |
-		uint64(s[28])<<32 |
-		uint64(s[29])<<40 |
-		uint64(s[30])<<48 |
-		uint64(s[31])<<56
-	return Item(s[__off0:__off1])
-}
-
-func (s Inventory) Slot3() Item {
-	_ = s[39]
-	var __off0 uint64 = uint64(s[24]) |
-		uint64(s[25])<<8 |
-		uint64(s[26])<<16 |
-		uint64(s[27])<<24 |
-		uint64(s[28])<<32 |
-		uint64(s[29])<<40 |
-		uint64(s[30])<<48 |
-		uint64(s[31])<<56
-	var __off1 uint64 = uint64(s[32]) |
-		uint64(s[33])<<8 |
-		uint64(s[34])<<16 |
-		uint64(s[35])<<24 |
-		uint64(s[36])<<32 |
-		uint64(s[37])<<40 |
-		uint64(s[38])<<48 |
-		uint64(s[39])<<56
-	return Item(s[__off0:__off1])
-}
-
-func (s Inventory) Slot4() Item {
-	_ = s[47]
-	var __off0 uint64 = uint64(s[32]) |
-		uint64(s[33])<<8 |
-		uint64(s[34])<<16 |
-		uint64(s[35])<<24 |
-		uint64(s[36])<<32 |
-		uint64(s[37])<<40 |
-		uint64(s[38])<<48 |
-		uint64(s[39])<<56
-	var __off1 uint64 = uint64(s[40]) |
-		uint64(s[41])<<8 |
-		uint64(s[42])<<16 |
-		uint64(s[43])<<24 |
-		uint64(s[44])<<32 |
-		uint64(s[45])<<40 |
-		uint64(s[46])<<48 |
-		uint64(s[47])<<56
-	return Item(s[__off0:__off1])
-}
-
-func (s Inventory) Slot5() Item {
-	_ = s[55]
-	var __off0 uint64 = uint64(s[40]) |
-		uint64(s[41])<<8 |
-		uint64(s[42])<<16 |
-		uint64(s[43])<<24 |
-		uint64(s[44])<<32 |
-		uint64(s[45])<<40 |
-		uint64(s[46])<<48 |
-		uint64(s[47])<<56
-	var __off1 uint64 = uint64(s[48]) |
-		uint64(s[49])<<8 |
-		uint64(s[50])<<16 |
-		uint64(s[51])<<24 |
-		uint64(s[52])<<32 |
-		uint64(s[53])<<40 |
-		uint64(s[54])<<48 |
-		uint64(s[55])<<56
-	return Item(s[__off0:__off1])
-}
-
-func (s Inventory) Slot6() Item {
-	_ = s[63]
-	var __off0 uint64 = uint64(s[48]) |
-		uint64(s[49])<<8 |
-		uint64(s[50])<<16 |
-		uint64(s[51])<<24 |
-		uint64(s[52])<<32 |
-		uint64(s[53])<<40 |
-		uint64(s[54])<<48 |
-		uint64(s[55])<<56
-	var __off1 uint64 = uint64(s[56]) |
-		uint64(s[57])<<8 |
-		uint64(s[58])<<16 |
-		uint64(s[59])<<24 |
-		uint64(s[60])<<32 |
-		uint64(s[61])<<40 |
-		uint64(s[62])<<48 |
-		uint64(s[63])<<56
-	return Item(s[__off0:__off1])
-}
-
-func (s Inventory) Slot7() Item {
-	_ = s[71]
-	var __off0 uint64 = uint64(s[56]) |
-		uint64(s[57])<<8 |
-		uint64(s[58])<<16 |
-		uint64(s[59])<<24 |
-		uint64(s[60])<<32 |
-		uint64(s[61])<<40 |
-		uint64(s[62])<<48 |
-		uint64(s[63])<<56
-	var __off1 uint64 = uint64(s[64]) |
-		uint64(s[65])<<8 |
-		uint64(s[66])<<16 |
-		uint64(s[67])<<24 |
-		uint64(s[68])<<32 |
-		uint64(s[69])<<40 |
-		uint64(s[70])<<48 |
-		uint64(s[71])<<56
-	return Item(s[__off0:__off1])
-}
-
-func (s Inventory) Slot8() Item {
-	_ = s[79]
-	var __off0 uint64 = uint64(s[64]) |
-		uint64(s[65])<<8 |
-		uint64(s[66])<<16 |
-		uint64(s[67])<<24 |
-		uint64(s[68])<<32 |
-		uint64(s[69])<<40 |
-		uint64(s[70])<<48 |
-		uint64(s[71])<<56
-	var __off1 uint64 = uint64(s[72]) |
-		uint64(s[73])<<8 |
-		uint64(s[74])<<16 |
-		uint64(s[75])<<24 |
-		uint64(s[76])<<32 |
-		uint64(s[77])<<40 |
-		uint64(s[78])<<48 |
-		uint64(s[79])<<56
-	return Item(s[__off0:__off1])
-}
-
-func (s Inventory) Slot9() Item {
-	_ = s[87]
-	var __off0 uint64 = uint64(s[72]) |
-		uint64(s[73])<<8 |
-		uint64(s[74])<<16 |
-		uint64(s[75])<<24 |
-		uint64(s[76])<<32 |
-		uint64(s[77])<<40 |
-		uint64(s[78])<<48 |
-		uint64(s[79])<<56
-	var __off1 uint64 = uint64(s[80]) |
-		uint64(s[81])<<8 |
-		uint64(s[82])<<16 |
-		uint64(s[83])<<24 |
-		uint64(s[84])<<32 |
-		uint64(s[85])<<40 |
-		uint64(s[86])<<48 |
-		uint64(s[87])<<56
 	return Item(s[__off0:__off1])
 }
 
