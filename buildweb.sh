262fcb3277b1bb39dd394d99cfde1e9d35c910c6
lemon-mint <ice.lemons.mint@gmail.com>
2022-01-22 16:06:40 +0900

    buildweb.sh

--- buildweb.sh
+++ buildweb.sh
@@ -1,3 +1,5 @@
 GOOS=js GOARCH=wasm go build -o web/dist/app.wasm -v ./web/wasm
 wasm-opt -Oz -o web/dist/app.min.wasm web/dist/app.wasm
 go run ./web/uriencode application/wasm ./web/dist/app.min.wasm ./web/dist/app.wasm.base64
+
+go build ./web
