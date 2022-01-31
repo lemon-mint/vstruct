ca408ff9b3873353d1fda0ccda9ea2a02b1d42a4
lemon-mint <ice.lemons.mint@gmail.com>
2022-01-31 19:00:47 +0900

    [backend/python] TypeConv

--- compile/backend/python/conv.go
+++ compile/backend/python/conv.go
@@ -7,31 +7,31 @@ func NameConv(in string) string {
 func TypeConv(in string) string {
 	switch in {
 	case "uint8":
-		return "U8"
+		return "int"
 	case "uint16":
-		return "U16"
+		return "int"
 	case "uint32":
-		return "U32"
+		return "int"
 	case "uint64":
-		return "U64"
+		return "int"
 	case "int8":
-		return "I8"
+		return "int"
 	case "int16":
-		return "I16"
+		return "int"
 	case "int32":
-		return "I32"
+		return "int"
 	case "int64":
-		return "I64"
+		return "int"
 	case "float32":
-		return "F32"
+		return "float"
 	case "float64":
-		return "F64"
+		return "float"
 	case "bool":
 		return "bool"
 	case "string":
-		return "String"
+		return "str"
 	case "bytes":
-		return "Uint8List"
+		return "bytearray"
 	default:
 		return NameConv(in)
 	}
