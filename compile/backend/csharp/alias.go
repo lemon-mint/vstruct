b30c91403e9547194323afe0f8b63c218abe3d72
snowmerak <snowmerak@outlook.com>
2022-04-07 18:33:16 +0900

    Create alias.go

---
+++ compile/backend/csharp/alias.go
@@ -0,0 +1,14 @@
+package csharp
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/lemon-mint/vstruct/ir"
+)
+
+func writeAliases(w io.Writer, i *ir.IR) {
+	for _, a := range i.Aliases {
+		fmt.Fprintf(w, "\tusing %s = %s;\n", NameConv(a.Name), TypeConv(a.OriginalType))
+	}
+}
